aa5fe9a3c580c60817a53fbab1110b0cde78c900
pinmacaroon <mark.pridalko@gmail.com>
2025-04-05 23:50:13 +0200

    commit

--- res/index.js
+++ res/index.js
@@ -1,8 +1,3 @@
-function pinclick() {
-    //messagebox("haii >_<");
-    messagebox("<img src='https://media.tenor.com/LwfpUq5ZagUAAAAj/spin-it-dancing.gif'>");
-}
-
 function closemsgbox() {
     document.getElementById("messagebox").style.visibility = "hidden";
     document.getElementById("messageboxtext").innerHTML = "";
